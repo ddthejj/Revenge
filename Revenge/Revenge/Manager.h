044b4f24cd87ee03f28a715e96086842a6ed910d
ddthejj <cooleaddento@gmail.com>
2019-03-23 03:38:38 -0400

    Manager.h fully commented

--- Revenge/Revenge/Manager.h
+++ Revenge/Revenge/Manager.h
@@ -22,88 +22,131 @@ public:
 
 #pragma region ENUMS 
 
-	enum TILES
+	
+	enum TILES				// Tiles
 	{
-		TILE_BROWNFLOOR,
-		TILE_REDWALL,
-		TILE_GREENDOOR,
+		TILE_BROWNFLOOR,	// Test floor
+		TILE_REDWALL,		// Test wall
+		TILE_GREENDOOR,		// Test door
 		TILE_MAX
 	};
 
-	enum TEXTURES
+	enum TEXTURES			// Textures
 	{
-		TEX_BLACK,
-		TEX_MENU,
-		TEX_ARROW,
-		TEX_BROWNFLOOR,
-		TEX_REDWALL,
-		TEX_GREENDOOR,
-		TEX_PLAYER,
+		TEX_BLACK,			// 1 black pixel
+		TEX_MENU,			// Menus borders, corners, and backgrounds
+		TEX_ARROW,			// Menu arrow
+		TEX_BROWNFLOOR,		// Test floor 
+		TEX_REDWALL,		// Test wall
+		TEX_GREENDOOR,		// Test door
+		TEX_PLAYER,			// Test Player spritesheet
 		TEX_MAX
 	};
 
-	enum KEYS
+	enum KEYS				// Keybinds
 	{
-		KEY_UP,
-		KEY_DOWN,
-		KEY_LEFT,
-		KEY_RIGHT,
-		KEY_MENU,
-		KEY_INTERACT,
+		KEY_UP,				// Up key
+		KEY_DOWN,			// Down key
+		KEY_LEFT,			// Left key
+		KEY_RIGHT,			// Right key
+		KEY_MENU,			// Open menu
+		KEY_INTERACT,		// Select / interact with tiles
 		KEY_MAX
 	};
 
 #pragma endregion
 
 private:
-	static SpriteBatch* spriteBatch;
-	static Texture* textures[TEX_MAX];
-	static ProtoTile* protoTiles[TILE_MAX];
-	static std::vector<Sprite*> UpdateList;
-	static std::vector<Sprite*> DrawList;
-	static std::vector<Map*> maps;
-	static std::vector<Character*> party;
+	static SpriteBatch* spriteBatch;			// Renderer class
+	static Texture* textures[TEX_MAX];			// List of all loaded textures
+	static ProtoTile* protoTiles[TILE_MAX];		// List of all prototype tiles 
+	static std::vector<Sprite*> UpdateList;		// Items that are being updated
+	static std::vector<Sprite*> DrawList;		// Items that are being drawn
+	static std::vector<Map*> maps;				// List of all maps possible
+	static std::vector<Character*> party;		// List of party members
 
-	static Map* currentMap;
-	static Player* currentPlayer;
+	static Map* currentMap;						// Currently loaded map
+	static Player* currentPlayer;				// Current active player
 
-	static bool fadingIn, fadingOut;
-	static float fadeOpacity;
-	static Door* doorHit;
-	static Sprite* fadeRectangle;
+	static bool fadingIn, fadingOut;			// Whether or not the screen is being faded in or out
+	static float fadeOpacity;					// What the current opacity of the fade is
+	static Door* doorHit;						// The door that the player hit
+	static Sprite* fadeRectangle;				// The rectangle that covers the screen when fading
 
-	static void TransitionRoom();
+	static void TransitionRoom();				// Fade the screen out, load the new room, fade the screen in
 
 public:
 
+#pragma region GameManager Access
+
+	// Get a prototyle
 	static ProtoTile* GetProtoTile(int index);
+	// Get a room
 	static Room* GetRoom(int index);
+	// Get the current player
 	static Player* GetCurrentPlayer() { return currentPlayer; }
+	// Get the party list
 	static std::vector<Character*> GetParty() { return party; }
+	// Get the current room
 	static Room* GetCurrentRoom();
+	// Get a texture
 	static Texture* GetTexture(int index) { if (index < TEX_MAX) return textures[index]; else return nullptr; }
+
+#pragma endregion
+
+#pragma region InputManager Access
+	
+	// Check if a key is down this frame
 	static bool IsKeyDown(KEYS index);// { return keys[index]; }
+	// Check if a key was down last frame
 	static bool IsPreviousKeyDown(KEYS index);// { return previousKeys[index]; }
+	// Check if a key was just pressed this frame
 	static bool IsKeyPressed(KEYS index);
 
+#pragma endregion
+
+#pragma region Access from Main
+
+	// Update the mouse position
 	static void MoveMouse(HWND hwnd, LPARAM lParam);
+	// Update the keystates when a key is pressed
 	static void PressKey(WPARAM wParam);
+	// Update the keystates when a key is released
 	static void ReleaseKey(WPARAM wParam);
+	// Update the window size and position when the window is moved / resized
 	static void ResizeWindow(HWND hwnd);
 
+#pragma endregion
+
+#pragma region Methods
+
+	// Initialize the whole game
 	static void Init(HWND hwnd);
+	// Destroy the whole game
 	static void Clean();
+	// Add an object to the list of objects being updated
 	static bool AddUpdate(Sprite*that);
+	// Remove an object from the list of objects being updated
 	static bool RemoveUpdate(Sprite*that);
+	// Add an object to the list of objects being drawn
 	static bool AddDraw(Sprite*that);
+	// Remove an object from the list of objects being drawn
 	static bool RemoveDraw(Sprite*that);
 
+	// Center the camera on an object
 	static void CenterCamera(float x, float y);
+	// Indicate that the player has hit a door
 	static void HitDoor(Door* hit);
+	// Freeze the entire scene
 	static void FreezeScene();
+	// Unfreeze the entire scene
 	static void UnfreezeScene();
 
+	// Update every object in the update list
 	static void Update(float delta_time);
+	// Draw every ovject in the draw list
 	static void Draw();
+
+#pragma endregion
 };
 
